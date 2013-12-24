86d342e15e4056237b33d36700bcd02101663c13
Geoffrey Plitt <geoff@gweb.org>
2013-12-23 20:05:55 -0800

    fixed symlinks

--- megasearch.sh
+++ megasearch.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y git-core
 # checkout
 git clone https://github.com/pillone/usntssearch
 # symlink config
-sudo ln -s /vagrant/megasearch_custom_params.ini ~/usntssearch/NZBmegasearch/custom_params.ini
+sudo ln -s /vagrant/myconfigs/megasearch_custom_params.ini /home/vagrant/usntssearch/NZBmegasearch/custom_params.ini
 # run
 python usntssearch/NZBmegasearch/mega2.py daemon
 # it's now running on port 5000
